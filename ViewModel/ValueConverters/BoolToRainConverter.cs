cebededd1c907e08fb90a15603d8c856c05d22cb
RavenGoku <raven.goku@gmail.com>
2024-06-10 13:58:42 +0100

    Seimple Weather App for Training

---
+++ ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherCloneApp.ViewModel.ValueConverters
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = value is bool;
+            if (isRaining == true)
+            {
+                return "Currently raining!";
+            }
+            return "Currently not raining";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = value as string;
+            if (isRaining == "CurrentlyRaining")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
